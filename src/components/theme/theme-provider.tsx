a74a9385bf7085180672eb20c7d29194f2ad892c
Jefferson <jefferson.brandao@g.globo>
2023-12-23 21:16:34 -0300

    criando tabela de orders

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -23,7 +23,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 export function ThemeProvider({
   children,
   defaultTheme = 'system',
-  storageKey = 'vite-ui-theme',
+  storageKey = 'dashfood-theme',
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
