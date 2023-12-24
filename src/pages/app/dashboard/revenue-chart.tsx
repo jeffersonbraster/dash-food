20902632249f63c9aef59be7a488de3ab2c116ea
Jefferson <jefferson.brandao@g.globo>
2023-12-23 22:32:03 -0300

    criando grafico de receita do periodo

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,70 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors, { violet } from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '10/12', receita: 1200 },
+  { date: '11/12', receita: 400 },
+  { date: '12/12', receita: 1500 },
+  { date: '13/12', receita: 200 },
+  { date: '14/12', receita: 550 },
+  { date: '15/12', receita: 1111 },
+  { date: '16/12', receita: 890 },
+]
+
+const RevenueChart = () => {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no periodo
+          </CardTitle>
+          <CardDescription>Receita diária do periodo</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="receita"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default RevenueChart
